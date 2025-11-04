2bafb68d6c517085dc7523b765cc0e82d00041df
raphjerem@gmail.com <raphjerem@gmail.com>
2025-11-04 15:37:59 +0000

    commit margin / purchase bis

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -2,9 +2,8 @@ SELECT
     a.quantity,
     a.revenue,
     b.purchase_price,
-    quantity*purchase_price AS purchase_cost
-    CAST(revenue AS int64) AS revenue,
-    revenue-purchase_cost AS margin
+    quantity*purchase_price AS purchase_cost,
+    revenue-quantity*purchase_price AS margin
 FROM {{ ref('stg_raw__sales') }} AS a
 JOIN {{ ref('stg_raw__product') }} AS b
     ON a.products_id = b.products_id
